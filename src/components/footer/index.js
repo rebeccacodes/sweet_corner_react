b48bd043bae85285f8b4ae98f0bae745680c049d
Rebecca Martin <rebecca-martin@live.com>
2019-05-09 22:31:45 -0700

    added styling and content to footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,10 +1,22 @@
 import './footer.scss';
+import dots from '../../assets/images/dots-footer.png';
+import phone from '../../assets/images/phone.png';
 import React from 'react';
 
 export default props => {
+    const date = new Date().getFullYear();
     return (
-        <div>
-            <h1 className="center">FOOTER</h1>
+        <div className="footer">
+            <div className="footer-left">
+                <img src={dots} />
+            </div>
+            <div className="footer-center">
+                Copyright &copy; {date} Sweet Corner. All rights reserved.
+            </div>
+            <div className="footer-right">
+                <img className="phone" src={phone} />
+                800 264 2099
+            </div>
         </div>
     )
 }
